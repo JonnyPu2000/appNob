b61b73b45d5b69f87c978a27e50e55047ddc21df
Rudney Eduardo <rudneyeduardo.souza@gmail.com>
2020-11-26 17:37:20 -0300

    Classes finalizadas, falta somente instanciar um objeto delas pra recebar os dados de login

--- lib/Classes/servProf.dart
+++ lib/Classes/servProf.dart
@@ -1,3 +1,5 @@
+//Classe para os detalhes do serviço!
+
 class ServProf {
   String fam;
   String item;
@@ -5,4 +7,22 @@ class ServProf {
   double preco;
   String abrev;
   int codcab;
+
+  ServProf(
+      this.fam, this.item, this.codserv, this.preco, this.abrev, this.codcab);
+
+  factory ServProf.fromJson(dynamic json) {
+    return ServProf(
+        json['fam'] as String,
+        json['item'] as String,
+        json['codserv'] as int,
+        json['preco'] as double,
+        json['abrev'] as String,
+        json['codcab'] as int);
+  }
+
+  @override
+  String toString() {
+    return '{ ${this.fam}, ${this.item}, ${this.codserv}, ${this.preco}, ${this.abrev}, ${this.codcab}}';
+  }
 }
